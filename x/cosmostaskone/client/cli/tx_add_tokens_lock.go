337c8f7fe4a82f4e3c703e04ebb7fd8437dc44ba
David Sorm <david.sorm@gmail.com>
2021-09-07 22:59:23 +0200

    added coinsRaw.ParseCoins()

--- x/cosmostaskone/client/cli/tx_add_tokens_lock.go
+++ x/cosmostaskone/client/cli/tx_add_tokens_lock.go
@@ -1,16 +1,49 @@
 package cli
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/spf13/cobra"
 	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+	cosmosTypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dsorm/cosmostaskone/x/cosmostaskone/types"
 )
 
 var _ = strconv.Itoa(0)
+
+type coinRaw struct {
+	Amount string
+	Denom  string
+}
+
+type coinsRaw []coinRaw
+
+func (cr coinsRaw) ParseCoins() ([]*cosmosTypes.Coin, error) {
+	var amount int64
+	var err error
+	coins := make([]*cosmosTypes.Coin, 0, len(cr))
+
+	for _, v := range cr {
+		coin := cosmosTypes.Coin{}
+		amount, err = strconv.ParseInt(v.Amount, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		if v.Denom == "" {
+			return nil, errors.New("error: no denom specified")
+		}
+
+		coin.Denom = v.Denom
+		coin.Amount = cosmosTypes.NewInt(amount)
+		coins = append(coins, &coin)
+	}
+
+	return coins, nil
+}
 
 func CmdAddTokensLock() *cobra.Command {
 	cmd := &cobra.Command{
