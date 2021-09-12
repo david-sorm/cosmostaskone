8454732de3f9f6004a2bac85fe4690770172551d
David Sorm <david.sorm@gmail.com>
2021-09-12 21:28:25 +0200

    reimplement AddTokensLock() with linked lists

--- x/cosmostaskone/keeper/msg_server_add_tokens_lock.go
+++ x/cosmostaskone/keeper/msg_server_add_tokens_lock.go
@@ -3,41 +3,48 @@ package keeper
 import (
 	"context"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
-	"strconv"
-
+	cosmosTypes "github.com/cosmos/cosmos-sdk/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/dsorm/cosmostaskone/x/cosmostaskone/types"
 )
-
-const keyPrefix = "TokenLock-"
-
-func (k msgServer) SetTokensLockCount(ctx sdk.Context, count uint64) {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(keyPrefix))
-	byteKey := types.KeyPrefix("Count")
-	countString := strconv.FormatUint(count, 10)
-
-	store.Set(byteKey, []byte(countString))
-}
 
 func (k msgServer) AddTokensLock(goCtx context.Context, msg *types.MsgAddTokensLock) (*types.MsgAddTokensLockResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// create new store from sdk context
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(keyPrefix))
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.WithPrefix(""))
 
-	// generate the ID, (last id) + 1
-	count := k.GetTokensLockCount(ctx)
-	id := count + uint64(1)
+	// check if the account has the balance specified
+	address, err := cosmosTypes.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range msg.Balances {
+		if !k.bankKeeper.HasBalance(ctx, address, *v) {
+			return nil,
+				sdkerrors.Wrapf(
+					sdkerrors.ErrInsufficientFunds,
+					"The account (%v) doesn't have %v %v\n",
+					msg.Creator,
+					v.Amount,
+					v.Denom)
+		}
+	}
 
-	// create unique key from prefix and id
-	key := keyPrefix + strconv.FormatUint(id, 10)
+	lastNode := types.TokenLockStartNode(store, k.cdc).Last(store, k.cdc)
 
-	// convert msg to binary
-	value := k.cdc.MustMarshalBinaryBare(msg)
-	store.Set([]byte(key), value)
+	currentNode := types.TokenLockInternal{
+		ID:       "",
+		Creator:  msg.Creator,
+		Balances: msg.Balances,
+		NextNode: "",
+	}
+	currentNode.GenerateUniqueID(store)
+	lastNode.NextNode = currentNode.ID
 
-	k.SetTokensLockCount(ctx, count+1)
+	currentNode.Save(store, k.cdc)
+	lastNode.Save(store, k.cdc)
 
-	idString := strconv.FormatUint(id, 10)
-	return &types.MsgAddTokensLockResponse{Id: idString}, nil
+	return &types.MsgAddTokensLockResponse{Id: currentNode.ID}, nil
 }
