eb0a31672cbf397faa92bee2ee47e90bd5e30891
David Sorm <david.sorm@gmail.com>
2021-09-12 21:29:41 +0200

    reimplement ListAllTokenLocks() using linked lists

--- x/cosmostaskone/keeper/grpc_query_list_all_token_locks.go
+++ x/cosmostaskone/keeper/grpc_query_list_all_token_locks.go
@@ -3,32 +3,11 @@ package keeper
 import (
 	"context"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
-	"strconv"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dsorm/cosmostaskone/x/cosmostaskone/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
-func (k Keeper) GetTokensLockCount(ctx sdk.Context) uint64 {
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(keyPrefix))
-	byteKey := types.KeyPrefix("Count")
-	result := store.Get(byteKey)
-
-	// no result, no element
-	if result == nil {
-		return 0
-	}
-
-	// bytes -> uint64
-	count, err := strconv.ParseUint(string(result), 10, 64)
-	if err != nil {
-		panic("err while converting bytes to uint64")
-	}
-
-	return count
-}
 
 func (k Keeper) ListAllTokenLocks(goCtx context.Context, req *types.QueryListAllTokenLocksRequest) (*types.QueryListAllTokenLocksResponse, error) {
 	if req == nil {
@@ -38,23 +17,15 @@ func (k Keeper) ListAllTokenLocks(goCtx context.Context, req *types.QueryListAll
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// create new store from sdk context
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte(keyPrefix))
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.WithPrefix(""))
 
-	tokenLockCount := k.GetTokensLockCount(ctx)
+	tokensLockList := make([]*types.TokensLock, 0, 16)
 
-	tokensLockList := make([]*types.TokensLock, 0, tokenLockCount)
-
-	var msg types.MsgAddTokensLock
-	for i := uint64(1); i <= tokenLockCount; i++ {
-		key := keyPrefix + strconv.FormatUint(i, 10)
-		bz := store.Get([]byte(key))
-
-		msg = types.MsgAddTokensLock{}
-		k.cdc.MustUnmarshalBinaryBare(bz, &msg)
+	for tokenLock := types.TokenLockStartNode(store, k.cdc); len(tokenLock.NextNode) != 0; tokenLock.Next(store, k.cdc) {
 		tokensLockList = append(tokensLockList, &types.TokensLock{
-			Id:       strconv.FormatUint(i, 10),
-			Creator:  msg.Creator,
-			Balances: msg.Balances,
+			Id:       tokenLock.ID,
+			Creator:  tokenLock.Creator,
+			Balances: tokenLock.Balances,
 		})
 	}
 
