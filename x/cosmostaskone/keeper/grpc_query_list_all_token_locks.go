39bfc4115073054cdc61405bfb6d4dab68400e5c
David Sorm <david.sorm@gmail.com>
2021-09-17 22:38:07 +0200

    rewrote and optimized ListAllTokenLocks()

--- x/cosmostaskone/keeper/grpc_query_list_all_token_locks.go
+++ x/cosmostaskone/keeper/grpc_query_list_all_token_locks.go
@@ -2,43 +2,40 @@ package keeper
 
 import (
 	"context"
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dsorm/cosmostaskone/x/cosmostaskone/types"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"strings"
 )
 
 func (k Keeper) ListAllTokenLocks(goCtx context.Context, req *types.QueryListAllTokenLocksRequest) (*types.QueryListAllTokenLocksResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
-	}
-
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// create new store from sdk context
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.WithPrefix(""))
+	// store := prefix.NewStore(ctx.KVStore(k.storeKey), types.WithPrefix(""))
 
 	tokensLockList := make([]*types.TokensLock, 0, 16)
 
-	nextNodeExists := true
-	for tokenLock := types.TokenLockStartNode(store, k.cdc); nextNodeExists; tokenLock = tokenLock.Next(store, k.cdc) {
-		// for better readability
-		disabledOrStartNode := tokenLock.Disabled || tokenLock.ID == "StartNode"
-		nextNodeExists = len(tokenLock.NextNode) != 0
+	store := ctx.KVStore(k.storeKey)
+	iterator := sdk.KVStorePrefixIterator(store, types.WithPrefix(""))
 
-		if disabledOrStartNode && nextNodeExists {
+	var tl types.TokenLockInternal
+	bz := []byte("")
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		tl = types.TokenLockInternal{}
+		// skip stored last hashes
+		if len(strings.Split(string(iterator.Key()), "-")) == 2 {
 			continue
 		}
-
-		if disabledOrStartNode && !nextNodeExists {
-			break
+		bz = iterator.Value()
+		k.cdc.MustUnmarshalBinaryBare(bz, &tl)
+		if tl.Disabled {
+			continue
 		}
-
 		tokensLockList = append(tokensLockList, &types.TokensLock{
-			Id:       tokenLock.ID,
-			Creator:  tokenLock.Creator,
-			Balances: tokenLock.Balances,
+			Id:       tl.ID,
+			Creator:  tl.Creator,
+			Balances: tl.Balances,
 		})
 	}
 
