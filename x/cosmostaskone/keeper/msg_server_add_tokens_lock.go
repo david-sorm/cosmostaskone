6ae34135a9a763c5f5ec6988b48089641ed0e8a9
David Sorm <david.sorm@gmail.com>
2021-09-27 15:52:51 +0200

    fixed AddTokensLock()

--- x/cosmostaskone/keeper/msg_server_add_tokens_lock.go
+++ x/cosmostaskone/keeper/msg_server_add_tokens_lock.go
@@ -32,19 +32,10 @@ func (k msgServer) AddTokensLock(goCtx context.Context, msg *types.MsgAddTokensL
 	}
 
 	// convert []*cosmosTypes.Coin to []cosmosTypes.Coin
-	//coins := types.DereferenceCoinSlice(msg.Balances)
+	coinsUnsorted := types.DereferenceCoinSlice(msg.Balances)
+	coins := types.SortCoinsAlphabetically(coinsUnsorted)
 
-	// temporary workaround for problem that causes the transaction to fail when multiple coins are entered at once
-	tempCoins := make(cosmosTypes.Coins, 1, 1)
-	for _, v := range msg.Balances {
-		tempCoins[0] = *v
-		err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, creatorAddress, types.ModuleName, tempCoins)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	// err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, creatorAddress, types.ModuleName, cosmosTypes.Coins(coins))
+	err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, creatorAddress, types.ModuleName, coins)
 
 	if err != nil {
 		return nil, err
