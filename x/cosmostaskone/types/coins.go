cf041d19d0f9bd294e174af0caff1155d4e8d79d
David Sorm <david.sorm@gmail.com>
2021-09-27 15:52:17 +0200

    added SortCoinsAlphabetically()

--- x/cosmostaskone/types/coins.go
+++ x/cosmostaskone/types/coins.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	cosmosTypes "github.com/cosmos/cosmos-sdk/types"
 	"strconv"
+	"strings"
 )
 
 var _ = strconv.Itoa(0)
@@ -39,7 +40,7 @@ func (cr CoinsRaw) ParseCoins() ([]*cosmosTypes.Coin, error) {
 }
 
 func DereferenceCoinSlice(toDereference []*cosmosTypes.Coin) []cosmosTypes.Coin {
-	var dereferenced cosmosTypes.Coins = make([]cosmosTypes.Coin, 0, len(toDereference))
+	dereferenced := make([]cosmosTypes.Coin, 0, len(toDereference))
 	for _, v := range toDereference {
 		dereferenced = append(dereferenced, cosmosTypes.Coin{
 			Denom:  v.Denom,
@@ -47,4 +48,28 @@ func DereferenceCoinSlice(toDereference []*cosmosTypes.Coin) []cosmosTypes.Coin 
 		})
 	}
 	return dereferenced
+}
+
+func SortCoinsAlphabetically(input []cosmosTypes.Coin) []cosmosTypes.Coin {
+	coins := make([]cosmosTypes.Coin, len(input), len(input))
+	copy(coins, input)
+
+	sorted := false
+	res := 0
+
+	// just a simple sorting algo
+	// TODO if it's worth it, implement something more efficient, maybe insertion sort
+	for !sorted {
+		sorted = true
+
+		for i := 0; i < len(coins)-1; i++ {
+			res = strings.Compare(coins[i].Denom, coins[i+1].Denom)
+			// if coins[i].Denom is greater string alphabetically than coins[i+1].Denom, flip them
+			if res == 1 {
+				coins[i], coins[i+1] = coins[i+1], coins[i]
+				sorted = false
+			}
+		}
+	}
+	return coins
 }
